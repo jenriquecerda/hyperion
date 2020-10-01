0492538757c24b57a76cceee78433f4c24845940
Enrique Cerda <jenrique.cerda@gmail.com>
2020-10-01 15:58:37 -0500

    Upgraded to mongodb driver 3.12.7

--- mongo/project.clj
+++ mongo/project.clj
@@ -1,8 +1,8 @@
-(defproject jenriquecerda/hyperion-mongo "3.8.36"
+(defproject jenriquecerda/hyperion-mongo "3.12.7"
   :description "Mongo Datastore for Hyperion"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [hyperion/hyperion-api "3.7.1"]
-                 [org.mongodb/mongodb-driver "3.6.0"]]
+                 [org.mongodb/mongodb-driver "3.12.7"]]
 
   :profiles {:dev {:dependencies [[speclj "2.7.5"]]}}
   :test-paths ["spec/"]
